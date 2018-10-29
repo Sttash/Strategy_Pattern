7b190700bbf2456ffd7f6b05785f8b49be620306
Иван <risk94@outlook.com>
2018-10-29 17:46:05 +0500

    Добавьте файлы проекта.

---
+++ Strategy_Pattern/Ducks/DuckBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_Pattern
+{
+    public abstract class DuckBase
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Quack! Quack!");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("I can swim!");
+        }
+
+        public abstract void Display();
+    }
+}
---
+++ Strategy_Pattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleDuck simpleDuck = new SimpleDuck();
+            ExoticDuck exoticDuck = new ExoticDuck();
+            
+            simpleDuck.Display();
+            simpleDuck.Quack();
+            simpleDuck.Swim();
+
+            Console.WriteLine();
+
+            exoticDuck.Display();
+            exoticDuck.Quack();
+            exoticDuck.Swim();
+
+            Console.ReadKey();
+        }
+    }
+}
